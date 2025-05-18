e66882e73725b3e8a5cd40b8de3a5434930dab2e
Scacchetti <luiscacchetti07@gmail.com>
2025-05-17 22:49:27 -0300

    Responsividade tela Home

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 import { customLoad } from "./loading.js";
 import { toggleHambuguer } from "./hamburguer.js"
 
-toggleHambuguer();
-
 const btnDireciona = document.getElementById('btnDireciona');
+
 btnDireciona.addEventListener('click', () => {
   location.href = "https://www.google.com/maps?q=Avenida+Europa,+595,+São+Paulo";
 })
 
-
+toggleHambuguer();
 customLoad();
+
